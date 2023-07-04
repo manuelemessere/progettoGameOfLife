a4a6ea5fc78d7b8b1b5ac1d3c155563933e3340b
manuelemessere <84188696+manuelemessere@users.noreply.github.com>
2023-07-04 23:07:35 +0200

    Create progettoCombo.py

---
+++ progettoCombo.py
@@ -0,0 +1,22 @@
+'''
+Inserimento valori da utente (tre valori: lato, tempo, percentuale riempimento)
+Usare turtle (griglia e logica di movimento, colorazione, del gioco, ogni passo è creato con fill e goto) tkinter (interfaccia, bottoni, avvio, salvataggio, caricamento file)
+
+'''
+
+class script:
+    '''
+    Usare turtle
+    Controllo convalidazione dei dati, tramite eccezioni
+    la logica: inserimento valori da utente
+    creazione griglia, associata alla logica del movimento
+    '''
+    
+class GUI:
+    '''
+    estetica con tk
+    widget: pulsanti e avvio o caricamento
+    widget: label dove si vedre il gioco, con pulsanti inizia, pausa, ricomincia, stop/salva
+    widget: in fase di chiusura, "Sei sicuro di uscire dal gioco?"
+    eccezioni e gestione degli errori collegati ai pulsanti di riferimento
+    '''
