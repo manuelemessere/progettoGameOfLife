ecac99aba8a0b868b3a989efbc1bd2bc439063cf
manuelemessere <84188696+manuelemessere@users.noreply.github.com>
2023-07-06 21:21:11 +0200

    finalYGame.py

--- yGame.py
+++ yGame.py
@@ -1,325 +1,139 @@
-#Problema: segue la regola del gioco, ma va troppo veloce e si blocca
+'''codice implementato nuovamente mettendo la logica nella classe Script, l'interfaccia grafica nella classe GUI.
+Trovare una soluzione, nel file finale, di usare la classe Script con il modulo turtle con cui
+creare la griglia e i movimenti del gioco
+'''
 import tkinter as tk
 import random
 
-
-class GameOfLife:
-
+class Script:
     def __init__(self, rows, cols, cell_size=10, speed=1):
-        """
-        This function initializes the GameOfLife object.
-
-        Parameters
-        ----------
-        rows : int
-            The number of rows in the grid.
-        cols : int
-            The number of columns in the grid.
-        cell_size : int, optional
-            The size of each cell in the grid. The default is 10.
-        speed : int, optional
-            The speed of the game. The default is 1.
-
-        Returns
-        -------
-        None.
-        """
         self.rows = rows
         self.cols = cols
         self.cell_size = cell_size
         self.speed = speed
         self.grid = self.create_grid()
         self.window = tk.Tk()
-        self.canvas = tk.Canvas(self.window, width=self.cols*self.cell_size,
-                                height=self.rows*self.cell_size,  bg='white')
+        self.canvas = tk.Canvas(self.window, width=self.cols*self.cell_size, height=self.rows*self.cell_size, bg='white')
         self.canvas.pack()
         self.window.resizable(False, False)
         self.window.title('Game of Life')
 
         self.draw_grid()
         self.create_buttons()
-        self.create_label()
         self.window.mainloop()
 
-    def create_buttons(self):
-        """
-        Create the start, stop, reset and speed buttons.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.
-
-        Returns
-        -------
-        None.
-        """
-        # Create a start button
-        self.start_button = tk.Button(
-            self.window, text='Start', command=self.run)
-        # Create a stop button
-        self.stop_button = tk.Button(
-            self.window, text='Stop', command=self.window.destroy)
-        # Create a reset button
-        self.reset_button = tk.Button(
-            self.window, text='Reset', command=self.reset)
-        # Create a speed slider
-        self.speed_slider = tk.Scale(self.window, from_=1, to=100, orient=tk.HORIZONTAL,
-                                     label='Speed', command=self.change_speed)
-        self.speed_slider.set(self.speed)
-
-        self.start_button.pack()
-        self.stop_button.pack()
-        self.reset_button.pack()
-        self.speed_slider.pack()
-
-    def create_label(self):
-        """
-        Add label to show the generation.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.
-
-        Returns
-        -------
-        None.
-        """
-        self.generation = 0
-        self.label = tk.Label(
-            self.window, text=f'Generation: {self.generation}')
-        self.label.pack()
-
-    def update_generation(self):
-        """
-        Update the generation label.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.
-
-        Returns
-        -------
-        None.
-        """
-        self.label.config(text=f'Generation: {self.generation}')
-
     def create_grid(self):
-        """
-        Create the grid.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.
-
-        Returns
-        -------
-        grid : list
-            The grid.
-        """
         grid = []
-        for row in range(self.rows):  # Loop through the rows
+        for row in range(self.rows):
             grid.append([])
-            for col in range(self.cols):  # Loop through the columns
-                # Append a random number to the grid
+            for col in range(self.cols):
                 grid[row].append(random.randint(0, 1))
         return grid
 
     def draw_grid(self):
-        """
-        Draw the grid.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.
-
-        Returns
-        -------
-        None.
-        """
-        for row in range(self.rows):  # Loop through the rows
-            for col in range(self.cols):  # Loop through the columns
+        self.canvas.delete('all')
+        for row in range(self.rows):
+            for col in range(self.cols):
                 x1 = col * self.cell_size
                 y1 = row * self.cell_size
                 x2 = x1 + self.cell_size
                 y2 = y1 + self.cell_size
-                # If the cell is alive
                 if self.grid[row][col] == 1:
-                    self.canvas.create_rectangle(
-                        x1, y1, x2, y2, fill='orange', outline='black')
-                # If the cell is dead
+                    self.canvas.create_rectangle(x1, y1, x2, y2, fill='orange', outline='black')
                 else:
-                    self.canvas.create_rectangle(
-                        x1, y1, x2, y2, fill='white', outline='black')
+                    self.canvas.create_rectangle(x1, y1, x2, y2, fill='white', outline='black')
 
     def update(self):
-        """
-        Update the grid.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.
-
-        Returns
-        -------
-        None.
-        """
         new_grid = []
         for row in range(self.rows):
             new_grid.append([])
             for col in range(self.cols):
-                # Append the new cell state to the grid
                 new_grid[row].append(self.get_new_cell_state(row, col))
-        # Update the grid
         self.grid = new_grid
-        # Delete all the items on the canvas
-        self.canvas.delete('all')
-        # Draw the new grid
         self.draw_grid()
         self.window.after(500//self.speed, self.update)
-        # Update the generation
-        self.generation += 1
-        self.update_generation()
 
     def get_new_cell_state(self, row, col):
-        """
-        Get the new cell state.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.
-        row : int
-            The row of the cell.
-        col : int
-            The column of the cell.
-
-        Returns
-        -------
-        new_state : int
-            The new state of the cell.
-        """
-        # Get the number of neighbours
-        num_neighbours = self.get_num_neighbours(
-            row, col)
-        # If the cell is alive
+        num_neighbours = self.get_num_neighbours(row, col)
         if self.grid[row][col] == 1:
-            # If the cell has less than 2 neighbours
-            if num_neighbours < 2:
-                # The cell dies
+            if num_neighbours < 2 or num_neighbours > 3:
                 return 0
-            # If the cell has 2 or 3 neighbours
             elif num_neighbours == 2 or num_neighbours == 3:
-                # The cell stayes alive
                 return 1
-            elif num_neighbours > 3:
-                # The cell dies
-                return 0
-        # If the cell is dead
         else:
-            # If the cell has 3 neighbours
             if num_neighbours == 3:
-                # The cell becomes alive
                 return 1
-            # If the cell has less than 3 neighbours
             else:
-                # The cell stays dead
                 return 0
 
     def get_num_neighbours(self, row, col):
-        """
-        Get the number of neighbours.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.
-        row : int
-            The row of the cell.
-        col : int
-            The column of the cell.
-
-        Returns
-        -------
-        num_neighbours : int
-            The number of neighbours.
-        """
         num_neighbours = 0
-        for i in range(-1, 2):  # Loop through the rows
-            for j in range(-1, 2):  # Loop through the columns
-                # If the cell is itself
+        for i in range(-1, 2):
+            for j in range(-1, 2):
                 if i == 0 and j == 0:
-                    # Skip the cell
                     continue
-                # Calculate the neighbour row
                 neighbour_row = row + i
-                # Calculate the neighbour column
                 neighbour_col = col + j
-                # If the neighbour is out of bounds
                 if neighbour_row < 0 or neighbour_row >= self.rows or neighbour_col < 0 or neighbour_col >= self.cols:
-                    # Skip the cell
                     continue
-                # Add the neighbour to the number of neighbours
                 num_neighbours += self.grid[neighbour_row][neighbour_col]
         return num_neighbours
 
     def run(self):
-        """
-        Run the game.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.
-
-        Returns
-        -------
-        None.
-        """
         self.update()
 
     def reset(self):
-        """
-        Reset the game.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.  
-
-        Returns
-        -------
-        None.
-        """
         self.grid = self.create_grid()
-        self.canvas.delete('all')
         self.draw_grid()
-        # Reset the generation
-        self.generation = 0
-        self.update_generation()
 
+    def create_buttons(self):
+        self.start_button = tk.Button(self.window, text='Start', command=self.run)
+        self.start_button.pack(side='left')
+        self.stop_button = tk.Button(self.window, text='Stop', command=self.window.destroy)
+        self.stop_button.pack(side='left')
+        self.reset_button = tk.Button(self.window, text='Reset', command=self.reset)
+        self.reset_button.pack(side='left')
+        
     def change_speed(self, speed):
-        """
-        Change the speed of the game.
-
-        Parameters
-        ----------
-        self : GameOfLife
-            The GameOfLife object.  
-        speed : int
-            The speed of the game.
-
-        Returns
-        -------
-        None.
-        """
         self.speed = int(speed)
 
+class GUI:
+    def __init__(self, rows, cols, cell_size=10, speed=1):
+        self.window = tk.Tk()
+        self.canvas = tk.Canvas(self.window, width=cols*cell_size, height=rows*cell_size, bg='white')
+        self.canvas.pack()
+        self.window.resizable(False, False)
+        self.window.title('Game of Life')
 
-if __name__ == '__main__':
-    game = GameOfLife(25, 25, 10, 100)
+        self.game = Script(rows, cols, cell_size, speed)
+        self.draw_grid()
+        self.create_buttons()
+        self.create_label()
+        self.window.mainloop()
+
+    def draw_grid(self):
+        self.game.draw_grid()
+
+    def create_buttons(self):
+        self.start_button = tk.Button(self.window, text='Start', command=self.game.run)
+        self.start_button.pack(side='left')
+        self.stop_button = tk.Button(self.window, text='Stop', command=self.window.destroy)
+        self.stop_button.pack(side='left')
+        self.reset_button = tk.Button(self.window, text='Reset', command=self.game.reset)
+        self.reset_button.pack(side='left')
+        self.speed_slider = tk.Scale(self.window, from_=1, to=10, orient='horizontal', label='Speed', command=self.game.change_speed)
+        self.speed_slider.pack(side='left')
+
+    def create_label(self):
+        self.generation_label = tk.Label(self.window, text='Generation: 0')
+        self.generation_label.pack(side='bottom')
+
+    def update_generation(self):
+        self.generation_label.config(text='Generation: {}'.format(self.game.generation))
+        
+rows = 20
+cols = 20
+cell_size = 20
+speed = 1
+
+gui = GUI(rows, cols, cell_size, speed)
